dbd1bcb0f39eb4a9e791cbe5938367bd479670d0
Carl Scarth <91951049+carl-scarth@users.noreply.github.com>
2023-07-06 17:02:24 +0100

    added FileSeries class for tracking files

---
+++ FileSeries.py
@@ -0,0 +1,4 @@
+class FileSeries:
+    # Contains properties of a series of .csv files associated with a particular test
+    def __init__(self,folder):
+        self.folder = folder # Parent folder in which the data is stored
