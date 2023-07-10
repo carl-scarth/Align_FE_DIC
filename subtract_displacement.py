723e3c866c78284119b07559b71e2dcfd84150c7
Carl Scarth <91951049+carl-scarth@users.noreply.github.com>
2023-07-10 11:16:43 +0100

    completed subtract_displacement code

--- subtract_displacement.py
+++ subtract_displacement.py
@@ -1,40 +1,29 @@
-
-# Loads in DIC data and subtracts the displacement from each set of coordinates to retrieve 
-# the undeformed geometry.
-
 import os
 import pandas as pd
 from FileSeries import *
 
 def subtract_disp(Files):
-    # Subtracts the displacement from the coordinates of each dataset to retrieve the undeformed shape
+    # Subtract the displacement from the coordinates of each dataset to retrieve the undeformed shape
     # Files = FileSeries class containing info on the location of input csvs and desired output location
     # Define list of coordinate indices, and their corresponding displacement index
-    column_inds = [["x", "u"],["y", "v"],["z","w"]] 
+    column_inds = [["x", "u"],["y", "v"],["z","w"]] # index of coordinate and the corresponding displacement component
     filenames = os.listdir(Files.in_path)
     # Loop through all files in the folder
     for filename in filenames:
-        print(filename)
         src = os.path.join(Files.in_path, filename)
         # Load data from csv file
         in_data = pd.read_csv(src, sep = ",").dropna()
         # Subtract each of the displacement components from their corresponding coordinate, and 
         # create a new entry with the result
-        for inds in column_inds:
-            in_data[inds[0]+"_0"] = in_data[inds[0]] - in_data[inds[1]]
-
-        # Try to use the below code to insert the columns at a specidied index, rather than appending and shuffling
-        in_data.insert()
-        new_cols = {"x_proj": [0,6], "y_proj": [1,7],"z_proj":[2,8]} 
-        [cloud_data.insert(loc=value[1], column = key, value = pd.Series(xyz_proj[:,value[0]])) for key, value in new_cols.items()]    
-    # Re-shuffle the order of the columns to place the undeformed coordinates next to the deformed ones
-    new_order = ["x", "y", "z", "x_0", "y_0", "z_0", "u", "v", "w", "corrU", "corrV", "corrW", "e1", "e2"]
-    out_data = in_data[new_order]
+        for i, inds in enumerate(column_inds):
+            # Insert entry next to the coordinates
+            in_data.insert(loc = (3+i), column = inds[0]+"_0", value = (in_data[inds[0]] - in_data[inds[1]]))
     
-    # Write transformed data to a new csv file
-    out_data.to_csv(os.path.join(out_path, filename), sep = ",", index = False)
+        # Write transformed data to a new csv file
+        in_data.to_csv(os.path.join(Files.out_path, filename), sep = ",", index = False)
 
 if __name__ == "__main__":
-    folder = "..\\Failure\\Processed DIC Data\\Individual Fields of View\\Alvium Pair 03\\Export_2"
-    # folder = "..\\Failure\\Processed DIC Data\\Individual Fields of View\\Manta Camera Pair\\Export_2"
+    # folder = "..\\Failure\\Processed DIC Data\\Individual Fields of View\\Alvium Pair 03\\Export_2"
+    folder = "..\\Failure\\Processed DIC Data\\Individual Fields of View\\Manta Camera Pair\\Export_2"
     Files = FileSeries(folder=folder,in_sub_folder="Data_with_outliers",out_sub_folder="Data_subtracted_disp")
+    subtract_disp(Files)
