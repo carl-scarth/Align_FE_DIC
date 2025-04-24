e321c983a5df7d3c5b28901773166ce6bcc16378
Carl Scarth <carlscarth87@gmail.com>
2025-04-24 14:21:49 +0100

    added subscript kwarg for consistency with other methods

--- subtract_displacement.py
+++ subtract_displacement.py
@@ -1,12 +1,12 @@
 from FileSeries import *
 import numpy as np
 
-def subtract_disp(Files, column_labels = [["x", "u"],["y", "v"],["z","w"]]):
+def subtract_disp(Files, column_labels = [["x", "u"],["y", "v"],["z","w"]], subscript = "0"):
     # Subtract the displacement from the coordinates of each dataset to retrieve the undeformed shape
     # Files = FileSeries class containing info on the location of input csvs and desired output location
     # Define list of coordinate indices, and their corresponding displacement index
     # Column inds = list of coordinate labels and the corresponding displacement component
-    Files.apply_func_to_data(lambda x:subtract_columns(x,column_labels), labels = [labels[0] for labels in column_labels], out_sub = "0", message = "Subtracting displacement")
+    Files.apply_func_to_data(lambda x:subtract_columns(x,column_labels), labels = [labels[0] for labels in column_labels], out_sub = subscript, message = "Subtracting displacement")
 
 def subtract_columns(data, column_labels = [["x", "u"],["y", "v"],["z","w"]]):
 
@@ -18,9 +18,8 @@ def subtract_columns(data, column_labels = [["x", "u"],["y", "v"],["z","w"]]):
     return(new_cols)
 
 if __name__ == "__main__":
-    folder = "..\\Failure\\Processed DIC Data\\Individual Fields of View\\Alvium Pair 03\\Export_2"
-    # folder = "..\\Failure\\Processed DIC Data\\Individual Fields of View\\Manta Camera Pair\\Export_2"
-    Files = FileSeries(folder=folder,in_sub_folder="Extracted_qoi",out_sub_folder="Data_subtracted_disp_2")
+    folder = "input_output"
+    Files = FileSeries(folder=folder,in_sub_folder="input",out_sub_folder="output")
     Files.read_data()
     subtract_disp(Files)
     Files.dump()
