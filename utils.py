d53e04e09772d3f8d123c5c490641e10ce78cba5
Carl Scarth <carlscarth87@gmail.com>
2025-04-27 14:10:00 +0100

    packaged downsample_data into utils

--- utils.py
+++ utils.py
@@ -71,3 +71,17 @@ def model_force_from_json(in_json):
 
     model_force = [frame["RFs"][2] for frame in model_dict["Sample"][0]["Frame"]]
     return(model_force)
+
+def downsample_data(data, sort = False, sort_col = ["Element"], method = "uniform", rate = 2):
+    # Downsample the data to reduce sample size
+    n_data = data.shape[0]
+    # Sort into order of ascending element number such that points in close proximity are grouped
+    if sort:
+        data.sort_values(sort_col,inplace=True)
+    if method == "uniform":
+        # Take a uniform subset of the samples by selecting at regular intervals
+        data = data.iloc[range(0,n_data,rate)]
+    elif method == "random":
+        # randomly select the desired subset of the data
+        data = data.sample(frac = 1.0/rate)
+    return(data)
