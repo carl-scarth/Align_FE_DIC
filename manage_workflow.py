60afa2bcd8f1f8b322f3e82c8bd56c71b554a388
Carl Scarth <carlscarth87@gmail.com>
2025-04-28 11:47:39 +0100

    completed example workflow

--- manage_workflow.py
+++ manage_workflow.py
@@ -1,43 +1,64 @@
 from FileSeries import *
+from SurfaceMesh import *
 from rename_files import *
 from subtract_displacement import *
 from transform_coords import *
+from get_nat_coords import *
 
-folder = "..\\Failure\\Processed DIC Data\\Individual Fields of View\\Alvium Pair 03\\Export_2" # Parent folder where data is located
-in_sub_folder = "Raw_Data"
-out_sub_folder = "Processed_Data"
-# Perhaps pass instructions as a Dictionary?
-rename_file = True # Do the files have a suffix which needs to be removed for paraview to recognise as a file series
+# Main script for carrying out point cloud manipulations
 
-QoI = ["coor.X [mm]", 
-        "coor.Y [mm]", 
-        "coor.Z [mm]",
-        "disp.Horizontal Displacement U [mm]",
-        "disp.Vertical Displacement V [mm]",
-        "disp.Out-Of-Plane: W [mm]",
-        "strain.Strain-global frame: Exx [ ]",
-        "strain.Strain-global frame: Eyy [ ]",
-        "strain.Strain-global frame: Exy [ ]",
-        "strain.Maximum Principal Strain: EI [ ]",
-        "strain.Minimum Principal Strain: EII [ ]"]
-    
-new_names = ["x", "y", "z", "u", "v", "w", "Exx", "Eyy", "Exy", "EI", "EII"]
+# Define folders where input and output are to be read/written
+folder = "input_output" # Parent folder
+in_sub_folder = "input" # Subfolder where input point cloud data is stored
+out_sub_folder = "output" # Subfolder where output is to be written
+mesh_file_string = "input_output\\FE_mesh" # Identifier of node and element connectivity file
 
-# In general I'd rather have the loop contained outside of the functions. Think about the best way to implement that.
-# Possible define a file series object with various properties? e.g. file names
+# Create list of Quantities of Interest to Extract, and define updated column names
+QoI = ["x", "y", "z", "u", "v", "w", "DAQ_DAQCrossheadmm","DAQ_DAQForcekN"]
+QoI_renamed = ["x", "y", "z", "u", "v", "w", "Crosshead","Force"]
+
+# Load in transformation rotation matrix and translation vector from file
+transmat_file = os.path.join(folder, "transformation_matrix.txt")
+R, T = transmat_from_file(transmat_file)
+
+# Load in mesh and create mesh object, containing nodal coordinates and connectivities
+Mesh = SurfaceMesh(from_file = True, file_string=mesh_file_string)
+# Specify that the mesh is an ordered grid, with n_x elements in the x direction, 
+# and n_y elements in the y direction (don't do if mesh is unstructured, another
+# method is used instead)
+n_x = 84 # number of columns in the grid
+n_y = 54 # number of rows in the grid
+Mesh.define_struct_grid(n_x, n_y)
+
+# Create FileSeries object and read data from csvs
 test_data = FileSeries(folder=folder, in_sub_folder=in_sub_folder, out_sub_folder=out_sub_folder)
 test_data.read_data(sep=",")
-# Generally do below, unless there is only one instruction in which case call the other function for renaming
-[remove_suffix(File) for File in test_data.files]
-test_data.extract_qoi(QoI, new_names=new_names, dropna = False)
-subtract_disp(test_data)
-# Load in a transformation matrix from file
-transmat_file = os.path.join(test_data.parent_path, "image_0000_transformation_matrix.txt")
-R, T = transmat_from_file(transmat_file)
-# Labels of columns to which coordinate transformations are performed, and Boolean indicating which are displacements
-trans_coord_labels = [["x", "y", "z"],["x_0","y_0","z_0"],["u","v","w"]]
-is_displacement = [False, False, True]
-transform_coords(test_data, trans_coord_labels, is_displacement = is_displacement, R=R, T=T, subscript = "rot")
-test_data.dump()
 
-# Might want to print a progress bar?
+# Down sample across time to retain 1 in 2 of the point clouds
+test_data.down_sam(2, rename_files = True)
+
+# Extract the chosen quantities of interest, and rename columns
+test_data.extract_qoi(QoI, new_names=QoI_renamed, dropna = False)
+
+# Subtract displacements from the coordinates to back-calculate undeformed point cloud
+# Append subscript "0" to column labels of output
+subtract_disp(test_data, column_labels = [["x", "u"],["y", "v"],["z","w"]], subscript = "0")
+
+# Apply the coordinate transformations specified by rotation matrix R and translation vector T
+# to x,y & z, x0, y0 & z0, and u, v & w. The latter are displacements so only a rotation is applied
+transform_coords(test_data, [["x", "y", "z"],["x_0","y_0","z_0"],["u","v","w"]], 
+                 is_displacement = [False, False, True], R=R, T=T, subscript = "rot")
+
+# Project DIC points onto FE model surface, and map Cartesian coordinates onto
+# (isoparametric) element natural coordinates. Map "undeformed" coordinates to
+# ensure compatibility with the model where the displacement may differ
+# specify first_file_only = True to copy mapped coordinates from first file 
+# to other increments, to save time if points are identical across increments
+get_nat_coords(test_data, Mesh, in_sub = "0_rot", out_cols = [4, 9, 14, -1, -1, -1], proj_sub = "proj", first_file_only = False)
+
+# Filter the points to retain only points between 15.0 <= z_proj <= 405.0
+bound_list = ["z_proj < 15.0", "z_proj > 405.0"]
+test_data.filter_by_cond(bound_list, drop_cond = True)
+
+# Write output csvs
+test_data.dump(dropna=True, index = True, index_label = "point_index")
